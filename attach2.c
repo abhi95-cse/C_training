c75f27489cbfdfc6593b91761f69665bcb8d6b52
abhi95-cse <ap716490@gmail.com>
2025-11-21 11:48:37 +0530

    Add files via upload

---
+++ attach2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void names (char first[], char last[],char sw)
+{
+    int i = 0;
+    char full[100];
+    while (first[i]!= '\0') // adding first
+    {
+        full[i] = first[i];
+        i++;
+    }
+    full[i] = ' '; // adding space
+    i++;
+    int j = 0;
+    while (last[j]!= '\0') //adding last
+    {
+full[i]=last[j];
+i++;
+j++;
+
+    
+}
+full[i]=' \0';
+printf("%s\n" , full);
+int len = i+1;
+if (len<sw)
+printf("fits in the screen");
+else
+printf("does not fit in the screen!");
+}
+    void main()
+{
+    char first[100]="raj";
+    char last[100]="kumar";
+    char sw = 8;
+    names(first,last,sw);
+}
