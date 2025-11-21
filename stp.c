867d51f1d8497daaadd85bc1ff12261bb05a083a
abhi95-cse <ap716490@gmail.com>
2025-11-21 15:18:01 +0530

    Add files via upload

---
+++ stp.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct student
+{
+    char name[100];
+    int age;
+    float marks;
+
+};
+void main()
+{
+    struct student s1;
+    struct student *p= &s1;
+strcpy(p-> name, "akshaya");
+p-> age = 17;
+p-> marks = 89.9;
+printf("%d",p->age);
+
+}
+
+
+
+
+
+
+
+
